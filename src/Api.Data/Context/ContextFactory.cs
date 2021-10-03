bc7a78bf384f71eecf1c6b21cee36fbfbd571721
johnny <johnny.kuo@thinkpower-info.com>
2021-10-03 22:47:58 +0800

    Implementation Context and ContextFactory

---
+++ src/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            var connectionString = "Server=localhost;Port=3306;Database=DDD;Uid=root;Pwd=h1122330";
+            var optionBuilder = new DbContextOptionsBuilder<MyContext>();
+
+            optionBuilder.UseMySQL(connectionString);
+            return new MyContext(optionBuilder.Options);
+        }
+    }
+}
