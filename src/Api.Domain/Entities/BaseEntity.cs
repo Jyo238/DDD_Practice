b74fc97778f2a336b719a6f52ba41e70332aaad5
johnny <johnny.kuo@thinkpower-info.com>
2021-10-03 22:47:39 +0800

    Implementation Entity

---
+++ src/Api.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        private DateTime? _createAt;
+
+        public DateTime? CreateAt
+        {
+            get { return _createAt; }
+            set { _createAt = _createAt == null ? DateTime.Now : value; }
+        }
+        public DateTime? UpdateAt { get; set; }
+    }
+}
