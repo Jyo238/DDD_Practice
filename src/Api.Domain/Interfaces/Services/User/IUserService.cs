0880a532af11018a5f6ac8b4b95fa1becb14e889
Johnny Kuo <johhny.gu.tcg@gmail.com>
2021-10-05 11:25:31 +0800

    interface and implementation service

---
+++ src/Api.Domain/Interfaces/Services/User/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Services.User
+{
+    public interface IUserService
+    {
+        Task<Entities.User> Get(Guid id);
+        Task<IEnumerable<Entities.User>> GetAll();
+
+        Task<Entities.User> Post(Entities.User user);
+        Task<Entities.User> Put(Entities.User user);
+        Task<bool> Delete(Guid id);
+    }
+}
---
+++ src/Api.Service/Services/UserService.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Domain.Interfaces.Services.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    public class UserService : IUserService
+    {
+        private IRepository<User> _repository;
+        public UserService(IRepository<User> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+
+        public async Task<User> Get(Guid id)
+        {
+            return await _repository.SelectAsync(id);
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            return await _repository.SelectAsync();
+        }
+
+        public async Task<User> Post(User user)
+        {
+            if (user.Name == "TEST") return null;
+            return await _repository.InsertAsync(user);
+        }
+
+        public async Task<User> Put(User user)
+        {
+            return await _repository.UpdateAsync(user);
+        }
+    }
+}
