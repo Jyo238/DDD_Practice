ab6fe669cc8a4fd9fadc95673c06c2ddb700b57a
johnny <johnny.kuo@thinkpower-info.com>
2021-10-09 16:06:05 +0800

    add AutoMapper and profiles

---
+++ src/Api.CrossCutting/Mappings/DtoToModelProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Domain.Dtos.User;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossCutting.Mappings
+{
+    public class DtoToModelProfiles : Profile
+    {
+        public DtoToModelProfiles()
+        {
+            CreateMap<User,UserDto>().ReverseMap();
+            CreateMap<User, UserResponseDto>().ReverseMap();
+        }
+    }
+}
